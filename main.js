632ab3a16922b556396d3f4492b28e80bfd82d7e
mnavas <mnavas@fi.upm.es>
2021-09-08 21:56:11 +0200

    Add files via upload
    webpage

---
+++ main.js
@@ -0,0 +1,12 @@
+(function(){
+  mixitup(".container",{
+    "animation": {
+        "duration": 250,
+        "nudge": true,
+        "reverseOut": true,
+        "effects": "fade rotateY(90deg) stagger(30ms)"
+    }
+  });
+})()
+
+
